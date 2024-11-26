77f8f76f68791d7322abbb5f6539c91f5bffff16
TheDavidPele369 <Davidmarquesdesouza09006@gmail.com>
2024-11-26 10:09:27 -0300

    ATualização 03.12v
    Santos Futebol Clube está na Série A!

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual é o melhor jogador da história?',
+    'O Pelé.'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o valor de pi?',
+    '3,1415926535897932384626433832795028...'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual e a raiz de 2?',
+    '1,4142...'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual foi o maior clube rebaixado na história do campeonato brasileiro?',
+    'Santos Futebol Clube.'
+)
